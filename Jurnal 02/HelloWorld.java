28e90336e257fb09c0138e56b0c12c7a1dd2f25d
fadhlansyauqi <adhansyauqi@gmail.com>
2021-09-28 23:30:17 +0700

    HelloWorld.java

---
+++ Jurnal 02/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Nama : Muhammad Fadhlan Syauqi");
+		System.out.println("NIM : 6706213049");
+		System.out.println("Kelas : D3 RPLA 45-01");
+	
+	}
+}
